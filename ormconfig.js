047b3e6939716176f1f77c579a389c088b4c46d0
Gabriel Peres Lutz <gabriel.p.lutz@gmail.com>
2021-08-06 16:25:28 -0300

    populate datatabase

--- ormconfig.js
+++ ormconfig.js
@@ -13,6 +13,7 @@ module.exports = {
   },
   extra: {
     ssl: {
+      require:true,
       rejectUnauthorized: false,
     }
   }
--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import "./setup";
 import express from "express";
 import cors from "cors";
 import "reflect-metadata";
-
+import axios from "axios"
 import connectDatabase from "./database";
 import * as userController from "./controllers/userController";
 import * as pokemonController from "./controllers/pokemonController"
 import { validateSignupBody } from "./middlewares/validateEmail";
 import { validateToken } from "./middlewares/validateToken";
+import { getRepository } from "typeorm";
+import Pokemon from "./entities/Pokemon";
 
 
 const app = express();
@@ -19,6 +21,30 @@ app.post("/sign-in",validateSignupBody, userController.loginUser)
 app.get("/pokemons", validateToken, pokemonController.getPokemonsList)
 app.post("/my-pokemons/:id/add", validateToken, pokemonController.addPokemonToMyList)
 app.post("/my-pokemons/:id/remove", validateToken, pokemonController.removePokemonFromMyList)
+
+app.get("/populate", async (req,res)=>{
+  console.log("running...")
+  for(let i = 1; i < 200; i ++){
+    const result = await axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+    const newPokemon = {
+      id: result.data.order,
+      name: result.data.name,
+      number: result.data.order,
+      image: result.data.sprites.front_default,
+      weight: result.data.weight,
+      height: result.data.height,
+      baseExp: result.data.base_experience,
+      description: "",
+      inMyPokemons: false
+    }
+
+    const speciesResult = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${i}`)
+    newPokemon.description = speciesResult.data.flavor_text_entries[0].flavor_text.split("\n").join(" ")
+    const pokemon = getRepository(Pokemon).create(newPokemon)
+    const resultquery = await getRepository(Pokemon).save(pokemon)
+  }
+  res.send("OK")
+})
 
 export async function init () {
   await connectDatabase();
